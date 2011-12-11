9a830cb00136f309d15fa59b115a927e62972fe1
John DeBolt <jdlish24@gmail.com>
2011-12-10 23:03:04 -0500

    improved the readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+source 'http://rubygems.org'
+gem 'rails', '3.0.10'
+gem 'sqlite3', '1.3.3'
+
+group :development, :test do
+ gem 'rspec-rails',      ">= 2.0.0.beta"
+end
+
+group :test do
+ gem 'rspec-rails', '2.6.1' 
+ gem 'webrat', '0.7.1'
 end
